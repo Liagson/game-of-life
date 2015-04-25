d0e76a44f846732ad3aedfa4147705776e0e2e7f
Liagson <aliaga.alberto@gmail.com>
2015-04-25 18:22:18 +0200

    Inicializacion matrices

--- game.c
+++ game.c
@@ -4,9 +4,10 @@
 #define VIDA 'X'
 #define MUERTE 'O'
 
-char** matriz;
+char** matriz1;
+char** matriz2;
 
-int contador_vecinos(int x, int y){
+int contador_vecinos(int x, int y, char** matriz){
 	/* Devuelve el número de vecinos vivos */
 	int contador = 0;
 	if (matriz[x - 1][y - 1] == VIDA) contador++;
@@ -24,7 +25,19 @@ int contador_vecinos(int x, int y){
 }
 
 main(){
+	int tamaño = 20;
+	int turno = 0;
+	int i, j;
 
+	matriz1 = malloc(tamaño * sizeof(char*));
+	for(i = 0; i < tamaño; i++) matriz1[i] = malloc(tamaño);
+	matriz2 = malloc(tamaño * sizeof(char*));
+	for(i = 0; i < tamaño; i++) matriz2[i] = malloc(tamaño);
 
+	for(i = 0; i < tamaño; i++)
+		for(j = 0; j < tamaño; j++){
+			matriz1[i][j] = MUERTE;
+			matriz2[i][j] = MUERTE;
+		}
 
 }
