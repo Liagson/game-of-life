3af63ebff7f054a0b6281879f88f35a1fff27eec
Liagson <aliaga.alberto@gmail.com>
2015-04-25 18:13:08 +0200

    Función contador vecinos añadida

---
+++ game.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+#define VIDA 'X'
+#define MUERTE 'O'
+
+char** matriz;
+
+int contador_vecinos(int x, int y){
+	/* Devuelve el número de vecinos vivos */
+	int contador = 0;
+	if (matriz[x - 1][y - 1] == VIDA) contador++;
+	if (matriz[x][y - 1] == VIDA) contador++;
+	if (matriz[x + 1][y - 1] == VIDA) contador++;
+
+	if (matriz[x - 1][y + 1] == VIDA) contador++;
+	if (matriz[x][y + 1] == VIDA) contador++;
+	if (matriz[x + 1][y + 1] == VIDA) contador++;
+
+	if (matriz[x - 1][y] == VIDA) contador++;
+	if (matriz[x + 1][y] == VIDA) contador++;
+
+	return contador;
+}
+
+main(){
+
+
+
+}
