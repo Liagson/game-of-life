213d6d6076261a8189bb769b9471bcfd60c0afe9
Liagson <aliaga.alberto@gmail.com>
2015-04-25 18:40:56 +0200

    actualizaciones de matrizes (motor)

--- game.c
+++ game.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <unistd.h> //Necesario para el sleep
+#include <stdlib.h> //Necesario para el malloc
 
 #define VIDA 'X'
 #define MUERTE 'O'
@@ -27,6 +29,8 @@ int contador_vecinos(int x, int y, char** matriz){
 main(){
 	int tamaño = 20;
 	int turno = 0;
+	int turno_limite = 20;
+	int vecinos;
 	int i, j;
 
 	matriz1 = malloc(tamaño * sizeof(char*));
@@ -39,5 +43,35 @@ main(){
 			matriz1[i][j] = MUERTE;
 			matriz2[i][j] = MUERTE;
 		}
+	
+	initscr();	
+	while(turno < turno_limite){
+		if(turno % 2){
+			for(i = 0; i < tamaño; i++)
+				for(j = 0; j < tamaño; j++){
+					vecinos = contador_vecinos(i, j, matriz2);
+					if (matriz2[i][j] == VIDA)						
+						if (vecinos > 3 ||  vecinos < 2) matriz1[i][j] = MUERTE;
+						else matriz1[i][j] = VIDA;
+					else
+						if (vecinos == 3) matriz1[i][j] = VIDA;
+						else matriz1[i][j] = MUERTE;					
+				}
+				
+		}else{
+			for(i = 0; i < tamaño; i++)
+				for(j = 0; j < tamaño; j++){
+					vecinos = contador_vecinos(i, j, matriz1);
+					if (matriz1[i][j] == VIDA)						
+						if (vecinos > 3 ||  vecinos < 2) matriz2[i][j] = MUERTE;
+						else matriz2[i][j] = VIDA;
+					else
+						if (vecinos == 3) matriz2[i][j] = VIDA;
+						else matriz2[i][j] = MUERTE;	
+
+		}
+
+	}	
+
 
 }
